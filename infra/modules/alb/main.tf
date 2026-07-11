3900880665e3c165194a4809a797b107a84cb652
pranavhude <pranavhude20@gmail.com>
2026-07-11 19:16:53 +0530

    updatecode

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -102,3 +102,33 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
+
+################################################################################
+# ALB LOG BUCKET POLICY
+################################################################################
+
+data "aws_caller_identity" "current" {}
+
+data "aws_elb_service_account" "this" {}
+
+resource "aws_s3_bucket_policy" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          AWS = data.aws_elb_service_account.this.arn
+        }
+
+        Action = "s3:PutObject"
+
+        Resource = "${aws_s3_bucket.alb_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+      }
+    ]
+  })
+}
