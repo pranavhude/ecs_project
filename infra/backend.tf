8af6cd343e2f7dcf764d386962a307f0c47ba4da
pranavhude <pranavhude20@gmail.com>
2026-07-11 19:13:46 +0530

    updatecode

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ecs-prod-terraform-state-euw1"
     key            = "ecs-ec2-prod/terraform.tfstate"
-    region         = "eu-west-1"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "ecs-prod-terraform-locks"
   }
